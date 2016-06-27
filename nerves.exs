cee5a6dfff5a4f95e4279cf0630994b2d1e2299e
Justin Schneck <jschneck@mac.com>
2016-06-27 09:36:25 -0400

    Add Staging URL to package config

--- nerves.exs
+++ nerves.exs
@@ -9,7 +9,8 @@ config :nerves_system_linkit, :nerves_env,
   type: :system,
   version: version,
   mirrors: [
-    "https://github.com/nerves-project/nerves_system_linkit/releases/download/v#{version}/nerves_system_linkit-v#{version}.tar.gz"],
+    "https://github.com/nerves-project/nerves_system_linkit/releases/download/v#{version}/nerves_system_linkit-v#{version}.tar.gz",
+    "https://s3.amazonaws.com/nerves/artifacts/nerves_system_linkit-#{version}.tar.gz"],
   build_platform: Nerves.System.Platforms.BR,
   build_config: [
     defconfig: "nerves_defconfig",
