0f1f7af2bbc4a510a0e751efc258610d040e2d2c
Frank Hunleth <fhunleth@troodon-software.com>
2016-08-10 09:26:56 -0400

    Add missing busybox.config

--- mix.exs
+++ mix.exs
@@ -36,7 +36,8 @@ defmodule NervesSystemLinkit.Mixfile do
     [maintainers: ["Frank Hunleth", "Justin Schneck"],
      files: ["rootfs-additions", "LICENSE", "mix.exs", "nerves_defconfig",
        "nerves.exs", "README.md", "VERSION", "rootfs-additions", "Config.in",
-       "external.mk", "package", "post-createfs.sh", "fwup.conf", "linux-3.18.29"],
+       "external.mk", "package", "post-createfs.sh", "fwup.conf", "linux-3.18.29",
+       "busybox.config"],
      licenses: ["Apache 2.0"],
      links: %{"Github" => "https://github.com/nerves-project/nerves_system_linkit"}]
   end
