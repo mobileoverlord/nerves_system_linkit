9fb586dae295e02cd84dbb421f006d1c142e4fe7
Frank Hunleth <fhunleth@troodon-software.com>
2017-06-13 10:37:02 -0400

    Update to nerves_system_br v0.12.0 (BR 2017.05)

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule NervesSystemLinkit.Mixfile do
 
   defp deps do
     [{:nerves, "~> 0.5", runtime: false },
-     {:nerves_system_br, "~> 0.10.0", runtime: false },
+     {:nerves_system_br, "~> 0.12.0", runtime: false },
      {:nerves_toolchain_mipsel_unknown_linux_musl, "~> 0.10.0", runtime: false}]
   end
 
